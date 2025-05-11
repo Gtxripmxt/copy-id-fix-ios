11ebff88a9bb4dfc5f1dab3cddd7f88434c926d6
Gtxripmxt <gtxripmxt@gmail.com>
2025-05-11 17:54:09 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,9 @@ using namespace geode::prelude;
 #include <Geode/modify/InfoLayer.hpp>
 class $modify(MyInfoLayer, InfoLayer) {
         void onCopyLevelID(CCObject * sender) {
-            FLAlertLayer::create(
-                "Copy ID",
-                "Level ID copied to the clipboard",
-                "OK"
-             )->show();
+            Notification::create("Level ID copied to clipboard", NotificationIcon::Success)->show();
 
             if (!m_level) return;
-
             std::string idStr = std::to_string(m_level->m_levelID.value());
 
             auto written = clipboard::write(idStr);
